bd60b381120e9f2bf139e929f7e99a79521fc56b
LeopoldHsing <LeopoldHsing@gmail.com>
2024-07-08 17:39:21 -0500

    chore: refine footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,23 +7,23 @@ const Footer = () => {
   return (
       <footer className="bg-[#21305e]">
         {/*  Container  */}
-        <div className="max-w-[1600px] mx-auto px-6 pt-10 pb-24 md:px-28">
+        <div className="max-w-[1600px] mx-auto px-6 pt-10 pb-24 lg:px-28">
           {/*  Footer Flex Container  */}
           <div
-              className="flex flex-col items-center space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+              className="flex flex-col items-center space-y-6 lg:flex-row lg:space-y-0 lg:justify-between lg:items-start">
             {/*  Menu & Logo Container  */}
-            <div className="-mt-2 flex flex-col items-center space-y-8 md:items-start md:space-y-4">
+            <div className="-mt-2 flex flex-col items-center space-y-8 lg:items-start lg:space-y-4">
               {/*  Logo  */}
               <div className="h-8 mb-2">
                 <a href="/">
-                  <span className="font-bold text-white text-4xl text-center hover:cursor-pointer">
+                  <span className="font-bold text-white text-4xl text-center text-nowrap hover:cursor-pointer">
                     Quant Hub Community
                   </span>
                 </a>
               </div>
               {/*  Menu Container  */}
               <div
-                  className="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+                  className="flex flex-col items-center space-y-4 font-bold text-white lg:flex-row lg:space-y-0 lg:space-x-6 lg:ml-3">
                 {/*  Item 1  */}
                 <div className="h-10 group">
                   <a href="/about">About</a>
@@ -50,7 +50,7 @@ const Footer = () => {
             {/*  Social & Copy Container  */}
             <div className="flex flex-col items-start justify-between space-y-4 text-gray-500">
               {/*  icons Container  */}
-              <div className="flex items-center justify-center mx-auto space-x-4 md:justify-end md:mx-0">
+              <div className="flex items-center justify-center mx-auto space-x-4 lg:justify-end lg:mx-0">
                 {/*  Icon 1  */}
                 <div className="h-8 group">
                   <a href="#">
