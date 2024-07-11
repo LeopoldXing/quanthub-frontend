e5cc6af22df07dd5b661b6cbe960a061a6015c06
LeopoldHsing <LeopoldHsing@gmail.com>
2024-07-11 09:32:37 -0500

    feat: create add articles page layout and basic elements

--- src/components/forms/ArticleModificationForm.tsx
+++ src/components/forms/ArticleModificationForm.tsx
@@ -1,6 +1,9 @@
 import MuiRichTextEditor from "@/components/mui/RichTextEditor/MuiRichTextEditor.tsx";
-import { MenuButtonBold, MenuButtonItalic, MenuControlsContainer, MenuDivider, MenuSelectHeading } from "mui-tiptap";
+import { MenuControlsContainer } from "mui-tiptap";
 import EditorMenuControls from "@/components/mui/RichTextEditor/EditorMenuControls.tsx";
+import { exampleContent } from "@/lib/dummyData.ts";
+import { Box, TextField } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
 
 type ArticleModificationFormProps = {
   initialData: {
@@ -19,20 +22,43 @@ type ArticleModificationFormProps = {
   mode?: "create" | "update"
 }
 
-const ArticleModificationForm = ({ initialData, mode="create" }: ArticleModificationFormProps) => {
+const ArticleModificationForm = ({ initialData, mode = "create" }: ArticleModificationFormProps) => {
   return (
-      <div className="w-full">
-        <div className="w-full px-8 border-[1px] border-[#e0e0e0]">
+      <Box width="100%" component="form" noValidate autoComplete="off">
+        {/*  Title  */}
+        <Box width="100%">
+          <InputLabel htmlFor="title" size="small">Title</InputLabel>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="title"
+              name="title"
+              size="small"
+              autoFocus
+          />
+          <InputLabel htmlFor="sub_title" size="small">Sub Title*</InputLabel>
+          <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="sub_title"
+              name="subTitle"
+              size="small"
+          />
+        </Box>
+        {/*  Rich Text Editor  */}
+        <div className="w-full px-8 mt-5 border-[1px] border-[#e0e0e0]">
           <MuiRichTextEditor
               renderControls={() => (
                   <MenuControlsContainer>
                     <EditorMenuControls/>
                   </MenuControlsContainer>
               )}
-              initialContent={mode === "create" ? undefined : initialData.content}
+              initialContent={mode === "create" ? exampleContent : initialData.content}
           />
         </div>
-      </div>
+      </Box>
   );
 };
 
--- src/pages/ArticleCreationPage.tsx
+++ src/pages/ArticleCreationPage.tsx
@@ -1,10 +1,29 @@
 import ArticleModificationForm from "@/components/forms/ArticleModificationForm.tsx";
+import Button from "@mui/material/Button";
 
 const ArticleCreationPage = () => {
+  /*  preview  */
+  const handlePreviewButtonClick = () => {
+
+  }
+
+  /*  publish  */
+  const handlePublishButtonClick = () => {
+
+  }
+
+  /*  save draft  */
+
   return (
       <div className="w-full mx-auto pb-16 flex flex-col items-start justify-start">
         {/*  title  */}
-        <div className="text-4xl font-bold">Create Article</div>
+        <div className="w-full space-y-8 md:space-y-0 md:flex justify-between items-center">
+          <div className="text-4xl font-bold">Create Article</div>
+          <div className="flex flex-nowrap justify-start md:justify-center items-center gap-4">
+            <Button variant="outlined" onClick={handlePreviewButtonClick}>Preview</Button>
+            <Button variant="contained" onClick={handlePublishButtonClick}>Publish</Button>
+          </div>
+        </div>
         <div className="mt-10">
           <ArticleModificationForm mode="create" initialData={{author: {username: "lll"}}}/>
         </div>
