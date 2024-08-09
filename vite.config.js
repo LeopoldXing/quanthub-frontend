9482da551fb3e6ae680feb4cec601edcb7865a56
LeopoldHsing <LeopoldHsing@gmail.com>
2024-08-08 19:25:22 -0500

    build: update docker file and vite config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            find: './runtimeConfig',
+            replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    }
+});
